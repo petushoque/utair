09fa974d924df9dd298f3bea36f5fbec12ec07d3
Alexei Kubasov <kubasovalexei@yandex.ru>
2021-06-29 19:28:49 +0700

    feat: add redux to project

---
+++ src/redux/store.js
@@ -0,0 +1,39 @@
+import { createStore } from 'redux'
+
+const initialState = {
+    username: '',
+    phone: '',
+    email: ''
+}
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'CHANGE_USERNAME':
+            return { ...state, username: action.payload }
+        case 'CHANGE_PHONE':
+            return { ...state, phone: action.payload }
+    }
+    return state;
+}
+
+const store = createStore(reducer, initialState);
+
+console.log(store.getState())
+
+const changeUsername = {
+    type: 'CHANGE_USERNAME',
+    payload: 'TEST'
+}
+
+const changePhone = {
+    type: 'CHANGE_PHONE',
+    payload: 'TEST'
+}
+
+store.dispatch(changeUsername)
+
+console.log(store.getState())
+
+store.dispatch(changePhone)
+
+console.log(store.getState())
